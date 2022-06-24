196da960cac68f577dffedbdc7e248845f12a9d7
richardjim <richardiyevhobu@gmail.com>
2022-06-24 16:01:41 +0100

    init.

--- product.js
+++ product.js
@@ -2,72 +2,72 @@ const products = [{
         id: 0,
         name: "White Sneaker",
         price: 40.99,
-        imgSrc: "Images/white sneaker.webp",
+        imgSrc: "/white sneaker.webp",
     },
     {
         id: 1,
         name: "Bag",
         price: 12.99,
-        imgSrc: "Images/92910_1537476925.webp",
+        imgSrc: "/92910_1537476925.webp",
     },
     {
         id: 2,
         name: "Jeans Sleeve",
         price: 14.99,
-        imgSrc: "/Images/jean_sleeve.webp",
+        imgSrc: "/jean_sleeve.webp",
     },
     {
         id: 3,
         name: "Samsung Smartphone",
         price: 79.10,
-        imgSrc: "/Images/samsung.webp",
+        imgSrc: "/samsung.webp",
     },
     {
         id: 4,
         name: "Wireless Controller",
         price: 59.99,
-        imgSrc: "/Images/cursor.webp",
+        imgSrc: "/cursor.webp",
     },
     {
         id: 5,
         name: "Headset",
         price: 39.20,
-        imgSrc: "/Images/headset.webp",
+        imgSrc: "/headset.webp",
     },
     {
         id: 6,
         name: "Call Of Duty",
         price: 19.99,
-        imgSrc: "/Images/call of duty.webp",
+        imgSrc: "/call of duty.webp",
     },
     {
         id: 7,
         name: "Violin",
         price: 17.99,
-        imgSrc: "/Images/violin.webp",
+        imgSrc: "/violin.webp",
     },
     {
         id: 8,
         name: "Mens Vintage",
         price: 20.99,
-        imgSrc: "/Images/vintage.webp",
+        imgSrc: "/vintage.webp",
     },
     {
         id: 9,
         name: "Sweatshirt",
         price: 25.99,
-        imgSrc: "/Images/unisex sweatshirt.webp"
+        imgSrc: "/unisex sweatshirt.webp"
     },
     {
         id: 10,
         name: "Rubic Cube",
         price: 17.99,
-        imgSrc: "/Images/rubic cube.webp"
+        imgSrc: "/rubic cube.webp"
     },
     {
         id: 11,
         name: "Swirlchair",
         price: 25.99,
-        imgSrc: "/Images/swirlchair.webp"
+        imgSrc: "/swirlchair.webp"
     },
 ];
